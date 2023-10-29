a2422865641bbf79a4e970b1c39c48e946ee412a
lex23unwin <lex23unwin@gmail.com>
2023-10-29 14:52:19 -0500

    almost done with cart functinoality

--- ordernow.js
+++ ordernow.js
@@ -89,11 +89,11 @@ categories.forEach((eachCategory) =>
     let i = 0;
     const idOfCategoryContainer = eachCategory.getAttribute("id");
 
-    categoryContainers.forEach((eachCategoryContainer) => 
+    categoryContainers.forEach((eachCategoryEntry) => 
     {
-        const decrementButton = eachCategoryContainer.querySelector('.decrement-button');
-        let amountCounter = eachCategoryContainer.querySelector('.amount-counter');
-        const incrementButton = eachCategoryContainer.querySelector('.increment-button');
+        const decrementButton = eachCategoryEntry.querySelector('.decrement-button');
+        let amountCounter = eachCategoryEntry.querySelector('.amount-counter');
+        const incrementButton = eachCategoryEntry.querySelector('.increment-button');
 
         decrementButton.id = `${idOfCategoryContainer}-minus-${i}`;
         amountCounter.id = `${idOfCategoryContainer}-value-${i}`;
@@ -102,21 +102,50 @@ categories.forEach((eachCategory) =>
         decrementButton.style.cursor = "pointer";
         incrementButton.style.cursor = "pointer";
 
-        const itemName = eachCategoryContainer.querySelector(".menu-body-entry-title").textContent
+        const itemName = eachCategoryEntry.querySelector(".menu-body-entry-title").textContent
 
         decrementButton.addEventListener("click", () => 
         {
+            //stuff for ordernow display
             let value = parseInt(amountCounter.textContent);
             if (value >= 1)
             {
                 amountCounter.textContent = (value - 1).toString();
             }
+
+            //stuff for cart,
+            if (value == 1)
+            {
+                //then remove from cart
+                removeFromCart(eachCategoryEntry);
+            }
+            else if (value >= 2)
+            {
+                //just decrement the quantity in the cart
+                decrementCartItem(eachCategoryEntry);
+            } 
+
         })
 
         incrementButton.addEventListener("click", () => 
         {
+            //ordernow display
             let value = parseInt(amountCounter.textContent);
             amountCounter.textContent = (value + 1).toString();
+
+            //cart stuff
+            if (value == 0)
+            {
+                //add line in the cart for the item
+                //and set its quantity to be 1, of course
+                addToCart(eachCategoryEntry);
+            }
+            else if (value >= 1)
+            {
+                //just increment the quantity in the cart
+                incrementCartItem(eachCategoryEntry);
+            }
+
         })
 
         i += 1;
@@ -124,3 +153,100 @@ categories.forEach((eachCategory) =>
     })
 
 });
+
+
+function addToCart(eachCategoryEntry)
+{
+    const mainCartOrderContainer = document.querySelector(".main-cart-order-container");
+
+    const newCartEntry = document.createElement("div");
+    newCartEntry.classList.add("main-cart-entry");
+    newCartEntry.id = `main-cart-entry-${returnIncrementedID()}`;
+
+    const newCartEntryTitle = document.createElement("div");
+    const title = eachCategoryEntry.querySelector(".menu-body-entry-title").textContent;
+    newCartEntryTitle.textContent = title;
+
+    const newCartEntryQuantity = document.createElement("div");
+    newCartEntryQuantity.textContent = 1;
+
+    newCartEntry.appendChild(newCartEntryTitle);
+    newCartEntry.appendChild(newCartEntryQuantity);
+
+    mainCartOrderContainer.appendChild(newCartEntry);
+}
+
+function removeFromCart(eachCategoryEntry)
+{
+    let mainCartOrderContainer = document.querySelector(".main-cart-order-container");
+
+    const mainCartEntries = mainCartOrderContainer.querySelectorAll(".main-cart-entry");
+
+    mainCartEntries.forEach(eachEntry => 
+    {
+        const entryTitle = eachEntry.querySelector(".main-cart-entry-title").textContent;
+        const entryQuantity = eachEntry.querySelector(newCartEntryQuantity).textContent;
+
+        const eachCategoryEntryTitle = eachCategoryEntry.querySelector(".menu-cart-entry-title");
+
+        if (entryTitle === eachCategoryEntryTitle)
+        {
+            mainCartOrderContainer.removeChild(eachEntry);
+        }
+    })
+
+    returnDecrementedID();
+
+}
+
+function incrementCartItem(eachCategoryEntry)
+{
+    let mainCartOrderContainer = document.querySelector("main-cart-order-container");
+
+    const mainCartEntries = mainCartOrderContainer.querySelectorAll(".main-cart-entry");
+
+    mainCartEntries.forEach(eachEntry => {
+        const entryTitle = eachEntry.querySelector(".main-cart-entry-title").textContent;
+        const entryQuantity = eachEntry.querySelector(newCartEntryQuantity)
+        const eachCategoryEntryTitle = eachCategoryEntry.querySelector(".menu-cart-entry-title");
+
+        if (entryTitle === eachCategoryEntryTitle)
+        {
+            entryQuantity.textContent = parseInt(entryQuantity.textContent) + 1;
+        }
+
+    })
+}
+
+function decrementCartItem(eachCategoryEntry)
+{
+    let mainCartOrderContainer = document.querySelector("main-cart-order-container");
+
+    const mainCartEntries = mainCartOrderContainer.querySelectorAll(".main-cart-entry");
+
+    mainCartEntries.forEach(eachEntry => {
+        const entryTitle = eachEntry.querySelector(".main-cart-entry-title").textContent;
+        const entryQuantity = eachEntry.querySelector(newCartEntryQuantity).textContent;
+
+        const eachCategoryEntryTitle = eachCategoryEntry.querySelector(".menu-cart-entry-title");
+
+        if (entryTitle === eachCategoryEntryTitle)
+        {
+            entryQuantity -= 1;
+        }
+    })
+}
+
+let cartID = 1;
+
+function returnIncrementedID()
+{
+    cartID += 1;
+    return cartID;
+}
+
+function returnDecrementedID()
+{
+    cartID -= 1;
+    return cartID;
+}
